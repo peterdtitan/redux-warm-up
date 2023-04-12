45df77f6adfc94b79b2a060a653bb201eba4879e
Peter Okorafor <peterdtitan@gmail.com>
2023-04-12 23:06:03 +0300

    Finish warm up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+import Navbar from "./components/Navbar";
+import Modal from "./components/Modal"
+import CartContainer from './components/CartContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar/>
+      <CartContainer />
+    </main>)
 }
 export default App;
